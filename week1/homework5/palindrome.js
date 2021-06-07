9eead002c1bbbf3dbbac64e579ea0f646caa57f0
Mingjing Mao <Mingjing.Mao@phoenix.edu>
2021-06-06 17:13:48 -0700

    init4

---
+++ week1/homework5/palindrome.js
@@ -0,0 +1,22 @@
+/*
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+样例
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama"
+Example 2:
+
+Input: "race a car"
+Output: false
+Explanation: "raceacar"
+挑战
+O(n) time without extra memory.
+
+注意事项
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+ */
