0c16b9eba837b5d8685894d88f106f29af8086c0
mingjingmao <mao@mingjingmaos-MacBook-Pro.local>
2021-06-20 22:01:40 -0400

    week23

---
+++ week2&3/FindShortestPathMatrix.js
@@ -0,0 +1,14 @@
+/*
+ * matrix for example:
+ * 1 0 1 1 1
+ * 1 0 1 0 1
+ * 1 1 1 0 1
+ * 0 0 0 0 1
+ * from top left corner to bottom right corner
+ * find the shortest path
+ * 1 means path
+ * 0 means no path
+ * so there are 12's 1 in example return 12
+ * 
+ * input example: String[] matrix = {"10111", "10101", "11101", "00001"};
+ */
---
+++ week2&3/ShortestPath.js
@@ -0,0 +1,39 @@
+/*
+ * Given a knight in a chess board (a binary matrix with 0 as empty and 1 as barrier) with a source position, find the shortest path to a destination position, return the length of the route.
+Return -1 if destination cannot be reached.
+
+样例
+Example 1:
+
+Input:
+[[0,0,0],
+ [0,0,0],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] 
+Output: 2
+Explanation:
+[2,0]->[0,1]->[2,2]
+Example 2:
+
+Input:
+[[0,1,0],
+ [0,0,1],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] 
+Output:-1
+说明
+If the knight is at (x, y), he can get to the following positions in one step:
+
+(x + 1, y + 2)
+(x + 1, y - 2)
+(x - 1, y + 2)
+(x - 1, y - 2)
+(x + 2, y + 1)
+(x + 2, y - 1)
+(x - 2, y + 1)
+(x - 2, y - 1)
+注意事项
+source and destination must be empty.
+Knight can not enter the barrier.
+Path length refers to the number of steps the knight takes.
+ */
