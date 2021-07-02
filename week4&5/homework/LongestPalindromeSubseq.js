056dfb013bb558faf9a9e2257fdc6ee5ec074999
mao <mingjingmao@gmail.com>
2021-07-01 22:23:58 -0400

    week4

---
+++ week4&5/homework/LongestPalindromeSubseq.js
@@ -0,0 +1,15 @@
+/*
+ * Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+样例
+Example1
+
+Input: "bbbab"
+Output: 4
+Explanation:
+One possible longest palindromic subsequence is "bbbb".
+Example2
+
+Input: "bbbbb"
+Output: 5
+ */
---
+++ week4&5/homework/tringPermutation2.js
@@ -0,0 +1,16 @@
+
+/*
+ * Given a string, find all permutations of it without duplicates.
+
+样例
+Example 1:
+
+Input: "abb"
+Output:
+["abb", "bab", "bba"]
+Example 2:
+
+Input: "aabb"
+Output:
+["aabb", "abab", "baba", "bbaa", "abba", "baab"]
+ */
